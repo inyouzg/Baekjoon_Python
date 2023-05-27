085a44d44cbc064b4aad687391c46771e07f4905
inyouzg <piy125@naver.com>
2023-05-27 22:14:04 +0900

    first commit

---
+++ 10869.py
@@ -0,0 +1,9 @@
+A, B = input().split()
+A = int(A)
+B = int(B)
+
+print(A + B)
+print(A - B)
+print(A * B)
+print(A // B)
+print(A % B)
---
+++ 2742.py
@@ -0,0 +1,5 @@
+N = int(input())
+
+if 1 <= N <= 100000 :
+    for i in range(1, N+1) :
+        print(N+1-i)
